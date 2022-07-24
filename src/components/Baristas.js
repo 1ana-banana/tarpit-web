181a6e7c9432e886fa478fb62f9eefbe1a6524d3
Anastasia Han <anastasia.han@yahoo.com>
2022-07-23 22:26:51 -0400

    Changes to classNames

---
+++ src/components/Baristas.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Cakes.css';
+import './Baristas.css';
+import { baristas } from '../data/baristas';
+import Slide from 'react-reveal/Reveal';
+
+const Baristas = ({ id }) => {
+	return (
+		<div className="baristaContainer" id={id}>
+			<h1 className="cakeHeading">Our Baristas</h1>
+			<div className="cakeWrapper">
+				{baristas.map((barista, index) => (
+					<Slide
+						key={(barista, index)}
+						right={index % 2 === 1 ? true : false}
+						left={index % 2 === 0 ? true : false}
+					>
+						<div className="cakeCard">
+							<img src={barista.img} className="baristaImg" alt={barista.alt} />
+							<div className="cakeInfo">
+								<div className="protTitle">{barista.name}</div>
+							</div>
+						</div>
+					</Slide>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default Baristas;
