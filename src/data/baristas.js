5495b636e5d545178af1999f7eba5e91441be726
Anastasia Han <anastasia.han@yahoo.com>
2022-07-25 21:10:57 -0400

    Added baristas details

--- src/data/baristas.js
+++ src/data/baristas.js
@@ -6,10 +6,12 @@ export const baristas = [
 		img: barista1,
 		alt: 'Barista',
 		name: 'Anastasia',
+		detail: 'Say hi to Anastasia! You might have seen her around the cafe. Probably serving you coffee. Ana is the newest member of the Tarpit team. She rides, she codes, she is a founder of motorcycle commmunities Cool Club Moto NYC and Moto Baes NYC. And of course, she makes excellent coffee!'
 	},
 	{
 		img: barista2,
 		alt: 'Barista',
 		name: 'Elya',
+		detail:'Elya has been with us for 4 years now! She is an amazing baker and artist. Multi-talented artist. Look out for her delicious vegan cakes on the weekends.'
 	},
 ];
